b8ae7e77e8734db46f8393d8fa8ac6ca2440f579
Shubham Karkhile <76725794+shubhz6599@users.noreply.github.com>
2025-04-16 15:53:23 +0530

    Update Feedback.js

--- models/Feedback.js
+++ models/Feedback.js
@@ -1,6 +1,6 @@
 const feedbackSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    message: String,
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    message: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
   });
   
