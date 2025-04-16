06142a2fad6bd7ef342518019840820059254dc4
Shubham Karkhile <76725794+shubhz6599@users.noreply.github.com>
2025-04-16 12:03:52 +0530

    Commit For Model DOB Mobile

--- models/User.js
+++ models/User.js
@@ -14,6 +14,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  dob: {
+    type: Date, // Date of Birth
+    required: true
+  },
+  mobile: {
+    type: String, // Mobile Number
+    required: true
+  },
+  otp: String, // OTP for email verification
+  otpExpiry: Date,
   resetToken: String,
   resetTokenExpiry: Date
 });
