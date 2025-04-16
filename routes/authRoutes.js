ca7edeb97f1bc8af45415768cebff48afd2462f1
Shubham Karkhile <76725794+shubhz6599@users.noreply.github.com>
2025-04-16 12:50:51 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,9 +4,12 @@ const {
   login,
   forgotPassword,
   resetPassword,
-  verifyOtp
+  verifyOtp,
+  resendOtp,
+  getUserDetails,
+  updateUserDetails
 } = require('../controllers/authController');
-
+const authenticate = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -14,5 +17,8 @@ router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 router.post("/verifyOtp", verifyOtp);
+router.post("/resendOtp", resendOtp); // Resend OTP API
+router.get("/user", authenticate, getUserDetails); // Get user details
+router.put("/user", authenticate, updateUserDetails); // Update user details
 
 module.exports = router;
