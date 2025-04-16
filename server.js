4b43c0b014d57c3e6d149d0cd3015e04e37c405d
Shubham Karkhile <76725794+shubhz6599@users.noreply.github.com>
2025-04-16 10:10:24 +0530

    Update Cors server.js

--- server.js
+++ server.js
@@ -8,13 +8,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(
-  cors({
-    origin: ["*"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ['Content-Type', 'Authorization'] 
-  })
-);
+app.use(cors({
+ origin:'http://localhost:4200',
+ credentials:true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
