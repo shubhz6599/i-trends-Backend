9715257cabfa5e9b64324803396648a6569644d7
Shubham Karkhile <76725794+shubhz6599@users.noreply.github.com>
2025-04-23 16:38:01 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getCart, addToCart, removeFromCart } = require('../controllers/cartController.js');
-const { placeOrder, getMyOrders, trackOrder } = require('../controllers/orderController.js');
+const { placeOrder,getOrdersByUser, getOrderDetailsById, getMyOrders, trackOrder } = require('../controllers/orderController.js');
 const { submitFeedback, getMyFeedback } = require('../controllers/feedbackController.js');
 const { getAllOrders, getAllFeedback, exportOrdersToExcel, updateOrderStatus } = require('../controllers/adminController.js');
 const authenticate = require('../middleware/authMiddleware.js');
@@ -14,6 +14,8 @@ router.post('/cart', authenticate, addToCart);
 router.delete('/cart', authenticate, removeFromCart);
 
 router.post('/order', authenticate, placeOrder);
+router.get('/user-orders', authenticate, getOrdersByUser);
+router.get('/:orderId', authenticate, getOrderDetailsById);
 router.get('/orders', authenticate, getMyOrders);
 router.get('/track/:orderId', authenticate, trackOrder);
 
