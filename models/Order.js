a66ed78b3773b44b0cfeefe939dc78caba141dfd
Shubham Karkhile <76725794+shubhz6599@users.noreply.github.com>
2025-04-23 16:02:20 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -4,13 +4,18 @@ const orderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   items: [
     {
-      productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-      quantity: Number,
-      price: Number,
+      productId: { type: String, required: true },
+      name: { type: String, required: true },
+      price: { type: Number, required: true },
+      quantity: { type: Number, required: true },
+      variant: { type: String }, // Optional
+      imageUrl: { type: String }, // Optional
+      mainOption: { type: String }, // Optional
+      subOption: { type: String }, // Optional
     },
   ],
-  totalAmount: Number,
-  paymentId: String,
+  totalAmount: { type: Number, required: true },
+  paymentId: { type: String, required: true },
   status: {
     type: String,
     enum: ['processing', 'confirmed', 'shipped', 'deliveryday', 'complete'],
