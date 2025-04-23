44184e915609a9c5cd0cd79c79ac9d0a7a33ea3b
Shubham Karkhile <76725794+shubhz6599@users.noreply.github.com>
2025-04-23 10:12:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+    origin: "*", // Allow all origins (not recommended for production)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
