90457171c4f4f2e4903334544164ad78ed9f056c
Shubham Karkhile <76725794+shubhz6599@users.noreply.github.com>
2025-04-23 16:10:51 +0530

    Update TempOrder.js

--- models/TempOrder.js
+++ models/TempOrder.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 
 const tempOrderSchema = new mongoose.Schema({
   razorpay_order_id: { type: String, required: true },
-  productId: { type: String, required: true }, // Product ID
-  name: { type: String, required: true }, // Product name
-  price: { type: Number, required: true }, // Price of the product
-  quantity: { type: Number, required: true }, // Quantity of the product
-  variant: { type: String }, // Optional: Variant of the product
-  imageUrl: { type: String }, // Optional: Image URL of the product
-  mainOption: { type: String }, // Optional: Main option selected
-  subOption: { type: String }, // Optional: Sub option selected
-  amount: { type: Number, required: true }, // Total amount
+  productId: { type: String, required: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  variant: { type: String },
+  imageUrl: { type: String },
+  mainOption: { type: String },
+  subOption: { type: String },
+  amount: { type: Number, required: true },
   status: { type: String, enum: ['created', 'verified'], default: 'created' },
   createdAt: { type: Date, default: Date.now },
 });
