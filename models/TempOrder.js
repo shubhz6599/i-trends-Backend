129343433bd4f8bc2c33a681bebabe83d803eee2
Shubham Karkhile <shubhz6599@gmail.com>
2025-04-30 21:39:39 +0530

    temporder

--- models/TempOrder.js
+++ models/TempOrder.js
@@ -13,6 +13,11 @@ const tempOrderSchema = new mongoose.Schema({
   amount: { type: Number, required: true },
   status: { type: String, enum: ['created', 'verified'], default: 'created' },
   createdAt: { type: Date, default: Date.now },
+  productType: {
+    type: String,
+    enum: ['specs', 'eyewear', 'contact-lens'],
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('TempOrder', tempOrderSchema);
